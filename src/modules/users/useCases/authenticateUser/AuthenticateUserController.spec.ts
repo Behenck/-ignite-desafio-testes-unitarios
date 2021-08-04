1f2b243db6e18ee34adc57f3682def95d248bbc0
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2021-08-04 18:47:21 -0300

    Add integration tests to user module use cases

---
+++ src/modules/users/useCases/authenticateUser/AuthenticateUserController.spec.ts
@@ -0,0 +1,47 @@
+import request from 'supertest';
+import { Connection, getConnection } from 'typeorm';
+
+import { app } from '../../../../app';
+import createConnection from '../../../../database';
+
+let connection: Connection;
+
+describe('AuthenticateUserController', () => {
+  beforeAll(async () => {
+    connection = await createConnection('test-connection');
+
+    await connection.query('DROP TABLE IF EXISTS statements');
+    await connection.query('DROP TABLE IF EXISTS users');
+    await connection.query('DROP TABLE IF EXISTS migrations');
+
+    await connection.runMigrations();
+  });
+
+  beforeEach(async () => {
+    await connection.query('DELETE FROM statements');
+    await connection.query('DELETE FROM users');
+  });
+
+  afterAll(async () => {
+    const mainConnection = getConnection();
+
+    await connection.close();
+    await mainConnection.close();
+  });
+
+  it('should be able to authenticate a user', async () => {
+    await request(app).post('/api/v1/users').send({
+      name: 'Test user',
+      email: 'test@email.com',
+      password: '123456',
+    });
+
+    const response = await request(app).post('/api/v1/sessions').send({
+      email: 'test@email.com',
+      password: '123456',
+    });
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('token');
+  });
+});
---
+++ src/modules/users/useCases/createUser/CreateUserController.spec.ts
@@ -0,0 +1,41 @@
+import request from 'supertest';
+import { Connection, getConnection } from 'typeorm';
+
+import { app } from '../../../../app';
+import createConnection from '../../../../database';
+
+let connection: Connection;
+
+describe('CreateUserController', () => {
+  beforeAll(async () => {
+    connection = await createConnection('test-connection');
+
+    await connection.query('DROP TABLE IF EXISTS statements');
+    await connection.query('DROP TABLE IF EXISTS users');
+    await connection.query('DROP TABLE IF EXISTS migrations');
+
+    await connection.runMigrations();
+  });
+
+  beforeEach(async () => {
+    await connection.query('DELETE FROM statements');
+    await connection.query('DELETE FROM users');
+  });
+
+  afterAll(async () => {
+    const mainConnection = getConnection();
+
+    await connection.close();
+    await mainConnection.close();
+  });
+
+  it('should be able to insert a new user', async () => {
+    const response = await request(app).post('/api/v1/users').send({
+      name: 'Test user',
+      email: 'test@email.com',
+      password: '123456',
+    });
+
+    expect(response.status).toBe(201);
+  });
+});
